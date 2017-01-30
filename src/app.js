28734046d8caedb614741c057dbfb11695b82e8e
Paul WB <higgsbison@gmail.com>
2017-01-30 11:27:19 -0800

    add router

--- src/app.js
+++ src/app.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore,  applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
+
+import reducers from './reducers';
+import Router from './router';
 
 class App extends Component {
   render() {
     return (
-      <Provider store={createStore()}>
-        <View>
-          <Text>
-            Hello!
-          </Text>
-        </View>
+      <Provider store={createStore(reducers, {}, applyMiddleware(ReduxThunk))}>
+        <Router />
       </Provider>
     );
   }
